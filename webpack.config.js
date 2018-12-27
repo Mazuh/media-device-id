82d8cdb91aea52b1b26aadf4417049e678da85ee
Marcell Guilherme Costa da Silva <marcell@evolux.net.br>
2018-12-27 11:23:50 -0300

    transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'media-device-id.min.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
